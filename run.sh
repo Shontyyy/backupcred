3cc9db7daf324f377f6f3c7735210b5062a64577
Shontyyy <122144932+Shontyyy@users.noreply.github.com>
2023-01-27 18:43:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ docker build -t newimg .
 docker image ls 
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker container run -td --name webserver newimg
+docker container run -td --name webserver -p 8080:8080 newimg
 docker container ls -la
