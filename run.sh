b5fc7112846a2dbd43ecb7b6e114413bbde5f65a
Shontyyy <122144932+Shontyyy@users.noreply.github.com>
2023-01-28 15:14:28 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker build -t newimg .
-docker image ls 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker container run -td --name webserver -p 8080:8080 newimg
-docker container ls -la
+docker rm -f $(docker ps -a -q)
+docker container run -it --name webserver -p 8081:8080 newimg /bin/bash
+cd /bin/
+./stastup.sh
