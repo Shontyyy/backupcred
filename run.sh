7027bcfbc50ed4f38c5c075e22dd8f5932a73b81
Shontyyy <122144932+Shontyyy@users.noreply.github.com>
2023-03-11 18:27:37 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-ssh root@65.2.37.144
-sudo tar -zcvf jenkins_backup.tar.gz --exclude /var/lib/jenkins/workspace/backupjenkins /var/lib/jenkins
+ssh -tt root@65.2.37.144 '
+sudo tar -czvf /home/jenkins.tar.gz /var/lib/jenkins
+cd /home
+ls -la
+du -h -m /home/jenkins.tar.gz
+sudo scp /home/jenkins.tar.gz 13.232.246.74:/var/lib/jenkins/workspace/xyz/jenkins.tar.gz
+'
